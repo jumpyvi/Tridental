745098b392c5418d93b567936cd872c54f78b0be
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-10-19 15:27:12 -0400

    Use lxdm

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,9 +4,11 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf5 swap @gnome-desktop @cinnamon-desktop --exclude=firefox,eom,xfburn,pidgin,xawtv,hexchat,thunderbird,gnome-calculator,simple-scan,gnome-terminal,gnome-calendar,transmission,transmission-gtk,yelp -y
+dnf5 swap @gnome-desktop @cinnamon-desktop --exclude=firefox,eom,xfburn,pidgin,xawtv,hexchat,lightdm,thunderbird,gnome-calculator,simple-scan,gnome-terminal,gnome-calendar,transmission,transmission-gtk,yelp -y
 
 dnf5 remove -y gnome-session gdm gnome-shell gnome-tweaks gnome-software gnome-tour evince || true
 
-systemctl enable lightdm
+dnf5 install xorg-x11-server-Xorg lxdm -y
+
+systemctl enable lxdm
 systemctl set-default graphical.target
