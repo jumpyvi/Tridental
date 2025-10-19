c69bb46c6393db08390785d2c58842877d1e8316
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-10-19 15:30:05 -0400

    Remove slick-greeter

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf5 swap @gnome-desktop @cinnamon-desktop --exclude=firefox,eom,xfburn,pidgin,xawtv,hexchat,lightdm,thunderbird,gnome-calculator,simple-scan,gnome-terminal,gnome-calendar,transmission,transmission-gtk,yelp -y
+dnf5 swap @gnome-desktop @cinnamon-desktop --exclude=firefox,eom,xfburn,pidgin,xawtv,hexchat,lightdm,slick-greeter-cinnamon,slick-greeter,thunderbird,gnome-calculator,simple-scan,gnome-terminal,gnome-calendar,transmission,transmission-gtk,yelp -y
 
 dnf5 remove -y gnome-session gdm gnome-shell gnome-tweaks gnome-software gnome-tour evince || true
 
