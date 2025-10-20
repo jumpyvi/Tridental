04e110b8ad36ab289669a45e01e1c724ed234f42
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-10-19 22:28:27 -0400

    Remove kvantum

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,7 @@ dnf5 swap @gnome-desktop @cinnamon-desktop --exclude=firefox,eom,xfburn,pidgin,x
 
 dnf5 remove -y gnome-session gdm gnome-shell gnome-tweaks gnome-software gnome-tour evince shotwell || true
 
-dnf5 install xorg-x11-server-Xorg kvantum evolution-ews-core cinnamon-calendar-server -y
+dnf5 install xorg-x11-server-Xorg evolution-ews-core cinnamon-calendar-server -y
 
 cp /ctx/system_files/usr/etc/ublue-lightdm-workaround.sh /etc/ublue-lightdm-workaround.sh
 cp /ctx/system_files/usr/lib/systemd/system/ublue-lightdm-workaround.service /usr/lib/systemd/system/ublue-lightdm-workaround.service
